6062fec5d0d21cdc63bdac0a4666c8dfff970aac
andre <toddot@live.ru>
2019-10-31 14:16:37 +0300

    Initial commit.

---
+++ build.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+cd $(dirname `which $0`)
+
+# Colors
+bold=$(tput bold)
+normal=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+blue=$(tput setaf 4)
+yellow=$(tput setaf 11)
+
+echo "Procedural brainfuck build system"
+
+
+error(){
+    echo "${red}(X): ${normal}${1}"
+}
+
+success(){
+    echo "${green}==> ${normal}${1}"
+}
+
+warn(){
+    echo "${yellow}==> ${normal}${1}"
+}
+
+info(){
+    echo "${blue}==> ${normal}${1}"
+}
+
+exec(){
+    info "${1}"
+    eval "${1}"
+    if [ ! $? -eq 0 ]; then
+        error "Exec: ${1} failed with non-zero exit code: ${?}"
+        exit -1
+    fi
+}
+
+require_directory(){ # Creates directory if not exist
+    if [ ! -d $1 ]; then
+        warn "Directory not found: ${1}"
+        exec "mkdir ${1}"
+    fi
+}
+
+change_dir(){
+    if [ ! -d $1 ]; then
+        error "No such directory: ${1}"
+        exit -1
+    fi
+    info "Entering directory: ${1}"
+    cd $1
+}
+
+leave_dir(){
+    info "Leaving directory: $(pwd)"
+    cd $BASEDIR
+}
+
+if [ $# -eq 0 ]; then
+    error "Please specify target. Use -h option for help."
+    exit -1
+fi
+
+if [[ $1 == "-h" ]]; then
+    echo "Usage:"$0" <-h> [debug|relaese|library|clean]"
+    echo "-h        Display this help message and exit."
+    echo "all       Build procedural brainfuck."
+    echo "clean     Remove obj/ bin/ directories."
+    exit 0
+fi
+### BEGIN BUILD SCRIPT ###
+BASEDIR=`pwd`
+CC="gcc"
+CFLAGS="-c"
+LD="gcc"
+LD_FLAGS=""
+OBJ_DIR="obj/"
+BIN_DIR="bin/"
+EXECUTABLE="pbf"
+declare -a SOURCES=("hashtable" "array" "main")
+
+target_clean(){
+   info "Cleaning up."
+   exec "rm -rf ${OBJ_DIR}"
+   exec "rm -rf ${BIN_DIR}"
+   success "Cleaned."
+}
+
+target_all(){
+   require_directory $OBJ_DIR
+   require_directory $BIN_DIR
+   info "Building procedural brainfuck..."
+   for file in "${SOURCES[@]}"
+   do
+       exec "${CC} ${CFLAGS} ${file}.c -o ${OBJ_DIR}${file}.o"
+   done
+   change_dir $OBJ_DIR
+   objects=$(printf " %s.o" "${SOURCES[@]}")
+   exec "${CC} -o ${BASEDIR}/${BIN_DIR}${EXECUTABLE} ${objects}"
+}
+
+### END BUILD SCRIPT ###
+
+if [[ `type -t "target_${1}"` == "function" ]]; then
+     eval "target_${1}"
+     success "Done."
+else
+     error "No such target:${1}."
+fi
---
+++ main.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <strings.h> //bzero
+#include <ctype.h>
+
+#include "hashtable.h"
+#include "array.h"
+
+#define ARRAY_SIZE 65535
+#define HASHTBL_SZ 255
+typedef struct{
+  int cp;
+  char *code;
+} procedure_t;
+
+bool is_regular(char c){ //regular chars are commands of pbf
+    return c=='>'||c=='<'||c=='['||c==']'||c=='+'||c=='-'||c=='!'||c==','||c=='.';
+}
+
+int *array;
+int ap;//array ptr
+
+bool eval_cmd(char cmd){
+         if(cmd=='+'){
+            array[ap]++;
+            return true;
+         }
+         if(cmd=='-'){
+            array[ap]--;
+            return true;
+         }
+         if(cmd=='>'){
+            ap++;
+            return true;
+         }
+         if(cmd=='<'){
+            ap--;
+            return true;
+         }
+         if(cmd=='.'){
+            printf("%c",array[ap]);
+            return true;
+         }
+         if(cmd==','){
+            scanf("%c",&array[ap]);
+            return true;
+         }
+         return false;
+}
+hashtable *funs;
+void eval(char *code){
+     array_t *loop_stack=array_create(1);
+     size_t csz = strlen(code);
+     size_t cp = 0;// code pointer
+     while(cp<csz){
+         char cmd=code[cp];
+         if(cmd==' '||cmd=='\n'){
+            ++cp;
+         }
+         if(eval_cmd(cmd)){
+            ++cp;
+            continue;
+         }
+         if(cmd=='['){
+            size_t *_cp=(size_t *)malloc(sizeof(size_t));//return addr for loop
+            *_cp=cp+1;
+            array_add(loop_stack, _cp);
+            ++cp;
+            continue;
+          }
+         if(cmd==']'){
+            if(array[ap]){
+               int ret = *(int*)loop_stack->base[loop_stack->sz-1];
+               cp=ret;
+            }else{
+               cp++;
+               array_pop(loop_stack);
+            }
+            continue;
+         }
+         if(cmd=='{'||cmd=='}'||cmd==';'){
+             printf("Unexpected token: `%c`\n",cmd);
+             exit(-1);
+         }
+         char *name=(char *)malloc(sizeof(char));
+         name[0]='\0';
+         size_t sz=1;
+         while(isalpha(code[cp])||code[cp]==' '){
+             if(code[cp]==' '){
+                 cp++;
+                 continue;
+             }
+             name[sz-1]=code[cp];
+             sz++;
+             name=(char *)realloc(name, sz*sizeof(char));
+             name[sz-1]='\0';
+             cp++;
+         }
+         if(code[cp]==';'){
+             procedure_t *proc = hashtbl_get(funs, name);
+             proc->cp=0;
+             eval(proc->code);
+             free(name);
+             ++cp;
+             continue;
+         }
+         if(code[cp]=='{'){
+             ++cp;
+             procedure_t *proc=(procedure_t *)malloc(sizeof(procedure_t));
+             proc->code=(char *)malloc(sizeof(char));
+             proc->code[0]='\0';
+             size_t sz=1;
+             do{
+                 proc->code[sz-1]=code[cp];
+                 ++sz;
+                 proc->code=(char*)realloc(proc->code, sz*sizeof(char));
+                 proc->code[sz-1]='\0';
+                 ++cp;
+             } while (code[cp]!='}');
+             hashtbl_add(funs, name, proc);
+             free(name);
+             ++cp;
+         }
+     }
+}
+int main(){
+    funs=hashtbl_create(HASHTBL_SZ);
+    array=(int *)malloc(sizeof(int)*ARRAY_SIZE);
+    char *code=(char *)malloc(sizeof(char));
+    code[0]='\0';
+    size_t sz=1;
+    for(;;){
+        char c=getc(stdin);
+        if(c==EOF){
+            break;
+        }
+        code[sz-1]=c;
+        code=realloc(code, sizeof(char)*(++sz));
+        code[sz-1]='\0';
+    }
+    bzero(array, sizeof(int)*ARRAY_SIZE);
+    eval(code);
+    free(code);
+    return  0;
+}
